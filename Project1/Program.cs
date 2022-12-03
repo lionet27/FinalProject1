6eae313635d80a9b89c939f2989610415d948941
«OlgaSoboleva27» <osoboleva27@gmail.com>
2022-12-04 00:22:23 +0300

    add metod StirngLess3Char

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] StringLess3Char(string[]array)
+
+{
+    int amountStringLess3char=0;
+    for(int i=0; i<array.Length;i++)
+        if((array[i]).Length<=3)
+            amountStringLess3char++;
+    string[] newArray=new string[amountStringLess3char];
+    int j=0;
+    for(int i=0; i<array.Length;i++)
+        if((array[i]).Length<=3)
+            {
+                newArray[j]=array[i];
+                Console.WriteLine(newArray[j]);
+                j++;
+            }
+    return newArray;
+}
+string[] myArray={"hello","2","world",":-)"}; 
+string[] string3Char=StringLess3Char(myArray);   
