ca891a25d17235f005fdfd429a0ff6cb83a3560c
«OlgaSoboleva27» <osoboleva27@gmail.com>
2022-12-04 00:51:28 +0300

    add metod ShowArray

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -11,10 +11,31 @@
         if((array[i]).Length<=3)
             {
                 newArray[j]=array[i];
-                Console.WriteLine(newArray[j]);
                 j++;
             }
     return newArray;
 }
+
+void ShowArray(string[]array)
+{
+        if(array.Length!=0)
+        {
+            Console.Write("Array is:[");
+
+            for(int i=0; i<(array.Length-1); i++)
+            {
+                Console.Write(array[i] + " ");
+                Console.Write(",");
+            }
+            Console.WriteLine(array[array.Length-1]+ "]");
+        }
+
+        else
+            Console.WriteLine("Array is:[]");
+}
+
 string[] myArray={"hello","2","world",":-)"}; 
+//string[] myArray={"Russia","Denmark","Kazan"}; 
+//string[] myArray={"1234","1567","-2","computer science"};
 string[] string3Char=StringLess3Char(myArray);   
+ShowArray(string3Char);
